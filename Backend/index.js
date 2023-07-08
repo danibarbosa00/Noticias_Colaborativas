20ceb7d5f8d24e4c06f67d08860d7a3f824147be
Alex Sunyer <alex.suner@gmail.com>
2023-07-08 22:42:49 +0200

    postman y css

--- Backend/index.js
+++ Backend/index.js
@@ -41,50 +41,65 @@ app.use(express.json());
 app.use(express.static("src/uploads"));
 
 //ENDPOINTS PÚBLICOS
+
 app.post("/usuario", nuevoUsuario);
+
 //localhost:3000/login --> LOGIN USUARIO
 app.post("/login", loginUsuario);
+
 //localhost:3000/noticias --> RUTA PARA TODAS LAS NOTICIAS
 app.get("/noticias", getNoticias);
+
 //localhost:3000/noticias/valoradas --> RUTA PARA LAS NOTICIAS ORDENADAS POR MÁS VALORADAS
 app.get("/noticias/valoradas", getNoticiasValoradas);
+
 //localhost:3000/noticias/:id/votos --> RUTA PARA SACAR VOTOS DE UNA NOTICIA
 app.get("/noticias/:id/votos", getVotosPorIdNoticia);
+
 //localhost:3000/noticias/:id --> RUTA PARA BUSCAR UNA NOTICIA POR ID DE LA NOTICIA
 app.get("/noticias/:id", getNoticiaPorId);
-//localhost:3000/noticias/:id --> RUTA PARA BUSCAR UNA NOTICIA POR EL NICKNAME DEL AUTOR O EL NOMBRE
-app.get("/noticias/usuarios/nombre/:nombre", getNoticiasNombre);
-//localhost:3000/noticias/:id --> RUTA PARA BUSCAR UNA NOTICIA POR EL NICKNAME DEL AUTOR O EL NOMBRE
-app.get("/noticias/usuarios/nickName/:nickName", getNoticiasNickName);
 
-///////////////////////////////////////////////////////////////////////
+//localhost:3000/noticias/:id --> RUTA PARA BUSCAR UNA NOTICIA POR EL NOMBRE DEL AUTOR
+app.get("/noticias/usuarios/nombre/:nombre", getNoticiasNombre);
+
+//localhost:3000/noticias/:id --> RUTA PARA BUSCAR UNA NOTICIA POR EL NICKNAME DEL AUTOR
+app.get("/noticias/usuarios/nickName/:nickName", getNoticiasNickName);
 
 //ENDPOINTS PRIVADOS
 
-app.get("/noticias/login/misnoticias", autentificarUsuario, getMisNoticias);
 //localhost:3000/misnoticias--->VER TUS NOTICIAS POR USUARIO ID
+app.get("/noticias/login/misnoticias", autentificarUsuario, getMisNoticias);
+
+//localhost:3000/usuario/:id --> MODIFICAR LOS DATOS DE UN USUARIO POR SU ID
 app.put("/usuario/:id", autentificarUsuario, updateUsuario);
-//localhost:3000/noticias/id/votar-->VOTAR LA NOTICIA POR SU ID
-app.delete("/usuario/:id", autentificarUsuario, deleteUsuario);
+
 ////localhost:3000/usuario/:id -->BORRAR UN USUARIO
+app.delete("/usuario/:id", autentificarUsuario, deleteUsuario);
+
 //localhost:3000/noticias --> CREAR NUEVA NOTICIA
 app.post("/noticias", autentificarUsuario, nuevaNoticia);
+
 //localhost:3000/noticias/:id --> MODIFICAR UNA NOTICIA POR SU ID
 app.put("/noticias/:id", autentificarUsuario, updateNoticia);
+
 //localhost:3000/noticias/:id --> BORRAR UNA NOTICIA POR SU ID
 app.delete("/noticias/:id", autentificarUsuario, deleteNoticia);
-//localhost:3000/usuario/:id --> MODIFICAR LOS DATOS DE UN USUARIO POR SU ID
+
+//localhost:3000/noticias/2/votar/positivo --> VOTAR NOTICIA POSITIVAMENTE
 app.post(
   "/noticias/:id/votar/positivo",
   autentificarUsuario,
   votarNoticiaPositiva
 );
+
+//localhost:3000/noticias/2/votar/negativo --> VOTAR NOTICIA NEGATIVAMENTE
 app.post(
   "/noticias/:id/votar/negativo",
   autentificarUsuario,
   votarNoticiaNegativa
 );
 
+//localhost:3000/noticias/votos/:id --> COMPROBAR EL ESTADO DEL VOTO DE UN USUARIO EN UNA NOTICIA
 app.get("/noticias/votos/:id", autentificarUsuario, comprobarVotoUsuario);
 
 //localhost:3000/noticias/:id/comentarios --> NUEVOCOMENTARIO DE UNA NOTICIA POR SU ID
@@ -99,12 +114,14 @@ app.delete(
   autentificarUsuario,
   deleteComentariosUsuario
 );
-//localjost:3000/miscomentarios ---> MIS COMENTARIOS
+
+//localhost:3000/miscomentarios ---> MIS COMENTARIOS
 app.get(
   "/noticias/login/miscomentarios/:id",
   autentificarUsuario,
   getMisComentarios
 );
+
 // Middleware de 404
 app.use((req, res) => {
   res.status(404).send({
