53ef3c76fcf9de17e500add4f9aade815c70c00a
Alex Sunyer <alex.suner@gmail.com>
2023-08-28 18:13:53 +0200

    register

--- Frontend/src/pages/register/Register.jsx
+++ Frontend/src/pages/register/Register.jsx
@@ -44,83 +44,81 @@ const Register = ({ handleRegisterToLogin }) => {
   const confirmPassword = watch("confirmPassword");
   const passwordsMatch = password === confirmPassword;
   return (
-    <div className="content-register">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-group">
-          <label className="labelRegister" htmlFor="nombre">
-            Nombre
-          </label>
-          <input
-            className="inputRegister"
-            type="text"
-            id="nombre"
-            minLength={MIN_LENGTH_NAME}
-            maxLength={MAX_LENGTH_NAME}
-            {...register("nombre", {
-              required: true,
-            })}
-          />
-        </div>
-        <div className="form-group">
-          <label className="labelRegister" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="inputRegister"
-            type="email"
-            id="email"
-            {...register("email", {
-              required: true,
-              pattern: {
-                value: EMAIL_REGEX,
-                message: "Ingrese un email válido",
-              },
-            })}
-          />
-          {error && <p className="errores-register">{error}</p>}
-        </div>
-        <div className="form-group">
-          <label className="labelRegister" htmlFor="password">
-            Contraseña
-          </label>
-          <input
-            className="inputRegister"
-            type="password"
-            id="password"
-            minLength={MIN_LENGTH_PASSWORD}
-            maxLength={MAX_LENGTH_PASSWORD}
-            {...register("password", {
-              required: true,
-              minLength: MIN_LENGTH_PASSWORD,
-              maxLength: MAX_LENGTH_PASSWORD,
-            })}
-          />
-        </div>
-        <div className="form-group">
-          <label className="labelRegister" htmlFor="confirmPassword">
-            Confirmar contraseña
-          </label>
-          <input
-            className="inputRegister"
-            type="password"
-            id="confirmPassword"
-            minLength={MIN_LENGTH_PASSWORD}
-            maxLength={MAX_LENGTH_PASSWORD}
-            {...register("confirmPassword", {
-              required: true,
-              minLength: MIN_LENGTH_PASSWORD,
-              maxLength: MAX_LENGTH_PASSWORD,
-            })}
-          />
-        </div>
-        {!passwordsMatch && (
-          <p className="errores-register">Las contraseñas no coinciden</p>
-        )}
-        <button className="registrate" type="submit">
-          Registrarse
-        </button>
-      </form>
-    </div>
+    <form className="form-register" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-group">
+        <label className="labelRegister" htmlFor="nombre">
+          Nombre
+        </label>
+        <input
+          className="inputRegister"
+          type="text"
+          id="nombre"
+          minLength={MIN_LENGTH_NAME}
+          maxLength={MAX_LENGTH_NAME}
+          {...register("nombre", {
+            required: true,
+          })}
+        />
+      </div>
+      <div className="form-group">
+        <label className="labelRegister" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="inputRegister"
+          type="email"
+          id="email"
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: EMAIL_REGEX,
+              message: "Ingrese un email válido",
+            },
+          })}
+        />
+        {error && <p className="errores-register">{error}</p>}
+      </div>
+      <div className="form-group">
+        <label className="labelRegister" htmlFor="password">
+          Contraseña
+        </label>
+        <input
+          className="inputRegister"
+          type="password"
+          id="password"
+          minLength={MIN_LENGTH_PASSWORD}
+          maxLength={MAX_LENGTH_PASSWORD}
+          {...register("password", {
+            required: true,
+            minLength: MIN_LENGTH_PASSWORD,
+            maxLength: MAX_LENGTH_PASSWORD,
+          })}
+        />
+      </div>
+      <div className="form-group">
+        <label className="labelRegister" htmlFor="confirmPassword">
+          Confirmar contraseña
+        </label>
+        <input
+          className="inputRegister"
+          type="password"
+          id="confirmPassword"
+          minLength={MIN_LENGTH_PASSWORD}
+          maxLength={MAX_LENGTH_PASSWORD}
+          {...register("confirmPassword", {
+            required: true,
+            minLength: MIN_LENGTH_PASSWORD,
+            maxLength: MAX_LENGTH_PASSWORD,
+          })}
+        />
+      </div>
+      {!passwordsMatch && (
+        <p className="errores-register">Las contraseñas no coinciden</p>
+      )}
+      <button className="registrate" type="submit">
+        Registrarse
+      </button>
+    </form>
   );
 };
 
